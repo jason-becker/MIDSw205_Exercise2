53ae8795f1c532419e8e50a0913121947b2e0f68
U-IPGNA\jason.becker <jason.becker@CHINSA-LW70854.na.corp.ipgnetwork.com>
2016-11-16 16:50:07 -0600

    reorganized files, moved start-tweetcount to within setup script

--- scripts/wordcount.py
+++ scripts/wordcount.py
@@ -2,15 +2,13 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
-
-
+import psycopg2
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
        
-
     def process(self, tup):
         word = tup.values[0]
 
@@ -19,8 +17,16 @@ class WordCounter(Bolt):
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
-        
-
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+        cur = conn.cursor()
+        if word in self.counts.keys():
+            #Update
+            cur.execute("UPDATE Tweetwordcount SET count=%s WHERE word=%s", (uWord, uCount))
+            conn.commit()
+        else:
+            #Insert
+            cur.execute("INSERT INTO Tweetwordcount (word,count) VALUES (" + word + ", 1)")
+            conn.commit()
         # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
